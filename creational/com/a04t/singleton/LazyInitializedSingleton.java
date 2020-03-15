2312625d68f00a16825766f4afb255e9d75d096f
Arpit Tripathi <arpit04tripathi@gmail.com>
2020-03-15 17:36:23 +0530

    singleton pattern

---
+++ creational/com/a04t/singleton/LazyInitializedSingleton.java
@@ -0,0 +1,13 @@
+package com.a04t.singleton;
+
+public class LazyInitializedSingleton {
+	private static LazyInitializedSingleton instance;
+
+	private LazyInitializedSingleton() {}
+
+	public static LazyInitializedSingleton getInstance() {
+		if (instance == null)
+			instance = new LazyInitializedSingleton();
+		return instance;
+	}
+}
---
+++ structural/com/a04t/adapter/Socket.java
@@ -0,0 +1,7 @@
+package com.a04t.adapter;
+
+public class Socket {
+	public Volt getVolt() {
+		return new Volt(120);
+	}
+}
